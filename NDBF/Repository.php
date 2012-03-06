b015fa11c27107b521785191d44ff97e4a29b734
OndrejSlamecka <ondrej@slamecka.cz>
2012-03-06 19:16:57 +0100

    Variables in pascalCase (BC break)

--- NDBF/Repository.php
+++ NDBF/Repository.php
@@ -15,119 +15,119 @@ namespace NDBF;
  */
 class Repository extends \Nette\Object
 {
-    /* ---------------------------- VARIABLES ------------------------------- */
+	/* ---------------------------- VARIABLES ------------------------------- */
 
-    /** @var \NDBF\RepositoryManager */
-    protected $parent;
+	/** @var \NDBF\RepositoryManager */
+	protected $parent;
 
-    /** @var \Nette\Database\Connection */
-    protected $connection;
+	/** @var \Nette\Database\Connection */
+	protected $connection;
 
-    /** @var string Associated table name */
-    protected $table_name;
-
-
-    /* ------------------------ CONSTRUCTOR, DESIGN ------------------------- */
-
-    public function __construct(RepositoryManager $parent, \Nette\Database\Connection $connection, $table_name = null)
-    {
-        $this->parent = $parent;
-        $this->connection = $connection;
-
-        // DATABASE TABLE NAME
-        if ($table_name === null) {
-            $table_name = get_class($this);
-            $table_name = substr($table_name, strrpos($table_name, '\\') + 1);
-        }
-        $this->table_name = strtolower($table_name); // Lowercase convention!
-    }
-
-    /* ---------------------- Nette\Database EXTENSION ---------------------- */
-
-    /**
-     * @return \Nette\Database\Table\Selection
-     */
-    final public function table()
-    {
-        return $this->connection->table($this->table_name);
-    }
-
-    /**
-     * @return \Nette\Database\Table\Selection
-     */
-    final public function select($columns = '*')
-    {
-        return $this->connection->table($this->table_name)->select($columns);
-    }
-
-    /**
-     * Returns all rows as an associative array.
-     * @param  string
-     * @param  string column name used for an array value or an empty string for the whole row
-     * @return array
-     */
-    public function fetchPairs($key, $val = '')
-    {
-        return $this->table()->fetchPairs($key, $val);
-    }
-
-    /**
-     * Counts table's rows.
-     * @param array,null $conditions
-     * @return int
-     */
-    public function count()
-    {
-        return $this->table()->count();
-    }
-
-    /**
-     * Deletes entity from db.
-     * @param array $conditions
-     * @throws LogicException, InvalidArgumentException
-     */
-    public function delete($conditions)
-    {
-        $this->connection->exec('DELETE FROM `' . $this->table_name . '` WHERE ', $conditions);
-    }
-
-    /**
-     * Saves record
-     * @param type $record
-     * @param type $table_id
-     */
-    public function save(&$record, $table_id)
-    {
-        // If there is no ID, we MUST insert
-        if (!isset($record[$table_id])) {
-            $insert = true;
-        } else {
-            // There is an ID
-            // Following condition allows restoring deleted items
-            if ($this->select($table_id)->where($table_id, $record[$table_id])->fetch()) // Is this entity already stored?
-                $insert = false; // Yes it is, so we'll update it
-            else
-                $insert = true; // No it isn't so insert
-        }
+	/** @var string Associated table name */
+	protected $tableName;
 
 
-        if ($insert) {
-            $this->connection
-                    ->exec('INSERT INTO `' . $this->table_name . '`', $record);
+	/* ------------------------ CONSTRUCTOR, DESIGN ------------------------- */
 
-            // Set last inserted item id
-            $record[$table_id] = $this->connection->lastInsertId();
-        }else
-            $this->connection
-                    ->exec('UPDATE `' . $this->table_name . '` SET ? WHERE `' . $table_id . '` = ?', $record, $record[$table_id]);
-    }
+	public function __construct(RepositoryManager $parent, \Nette\Database\Connection $connection, $tableName = null)
+	{
+		$this->parent = $parent;
+		$this->connection = $connection;
 
-    /* --- DEPRECATED --- */
+		// DATABASE TABLE NAME
+		if ($tableName === null) {
+			$tableName = get_class($this);
+			$tableName = substr($tableName, strrpos($tableName, '\\') + 1);
+		}
+		$this->tableName = strtolower($tableName); // Lowercase convention!
+	}
 
-    /** @deprecated */
-    final protected function getParent()
-    {
-        return $this->parent;
-    }
+	/* ---------------------- Nette\Database EXTENSION ---------------------- */
+
+	/**
+	 * @return \Nette\Database\Table\Selection
+	 */
+	final public function table()
+	{
+		return $this->connection->table($this->tableName);
+	}
+
+	/**
+	 * @return \Nette\Database\Table\Selection
+	 */
+	final public function select($columns = '*')
+	{
+		return $this->connection->table($this->tableName)->select($columns);
+	}
+
+	/**
+	 * Returns all rows as an associative array.
+	 * @param  string
+	 * @param  string column name used for an array value or an empty string for the whole row
+	 * @return array
+	 */
+	public function fetchPairs($key, $val = '')
+	{
+		return $this->table()->fetchPairs($key, $val);
+	}
+
+	/**
+	 * Counts table's rows.
+	 * @param array,null $conditions
+	 * @return int
+	 */
+	public function count()
+	{
+		return $this->table()->count();
+	}
+
+	/**
+	 * Deletes entity from db.
+	 * @param array $conditions
+	 * @throws LogicException, InvalidArgumentException
+	 */
+	public function delete($conditions)
+	{
+		$this->connection->exec('DELETE FROM `' . $this->tableName . '` WHERE ', $conditions);
+	}
+
+	/**
+	 * Saves record
+	 * @param type $record
+	 * @param type $tableId
+	 */
+	public function save(&$record, $tableId)
+	{
+		// If there is no ID, we MUST insert
+		if (!isset($record[$tableId])) {
+			$insert = true;
+		} else {
+			// There is an ID
+			// Following condition allows restoring deleted items
+			if ($this->select($tableId)->where($tableId, $record[$tableId])->fetch()) // Is this entity already stored?
+				$insert = false; // Yes it is, so we'll update it
+			else
+				$insert = true; // No it isn't so insert
+		}
+
+
+		if ($insert) {
+			$this->connection
+					->exec('INSERT INTO `' . $this->tableName . '`', $record);
+
+			// Set last inserted item id
+			$record[$tableId] = $this->connection->lastInsertId();
+		}else
+			$this->connection
+					->exec('UPDATE `' . $this->tableName . '` SET ? WHERE `' . $tableId . '` = ?', $record, $record[$tableId]);
+	}
+
+	/* --- DEPRECATED --- */
+
+	/** @deprecated */
+	final protected function getParent()
+	{
+		return $this->parent;
+	}
 
 }
