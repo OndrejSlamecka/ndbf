OndrejSlamecka/ndbf

commit b583a3d737eee0a95198050ae981cffbdd5b6dfa

Fixed CompilerExtension's interpretation of config

Ndbf/CompilerExtension.php
@@ -22,7 +22,7 @@ class CompilerExtension extends \Nette\Config\CompilerExtension
 		if (isset($config['repositories'])) {
 			foreach ($config['repositories'] as $name => $value) {
 
-				$className = isset($value['class']) ? $value['class'] : $value;
+				$className = is_array($value['class']) ? $value['class'] : $value;
 				$serviceDefinition = $builder->addDefinition($this->prefix('repositories.' . $name))
 						->setClass($className);
 

commit 2b3120b90f92e8c8ce7d8e25a405f0ceb2a109e2

Fixed the other way of writing config (bug introduced in b583a3d737) + test

Ndbf/CompilerExtension.php
@@ -22,7 +22,11 @@ class CompilerExtension extends \Nette\Config\CompilerExtension
 		if (isset($config['repositories'])) {
 			foreach ($config['repositories'] as $name => $value) {
 
-				$className = is_array($value['class']) ? $value['class'] : $value;
+				if (is_array($value) && !isset($value['class'])) {
+					throw new \InvalidArgumentException("Repository $name doesn't have defined 'class' parameter");
+				}
+
+				$className = is_array($value) ? $value['class'] : $value;
 				$serviceDefinition = $builder->addDefinition($this->prefix('repositories.' . $name))
 						->setClass($className);
 

tests/ExampleRepository2.php
@@ -1,0 +1,6 @@
+<?php
+
+class ExampleRepository2 extends Ndbf\Repository
+{
+
+}

tests/NDBF/RepositoryManagerTest.php
@@ -17,6 +17,7 @@ class RepositoryManagerTest extends PHPUnit_Framework_TestCase
         $rm = new \Ndbf\RepositoryManager($container);
         self::assertInstanceOf('Ndbf\Repository', $rm->getRepository('FooBarRepository'));
         self::assertInstanceOf('ExampleRepository', $rm->getRepository('Example'));
+		self::assertInstanceOf('ExampleRepository2', $rm->getRepository('Example2'));
     }
 
     /**

tests/bootstrap.php
@@ -24,6 +24,7 @@ $configurator->createRobotLoader()
 		->register();
 
 require_once __DIR__ . '/ExampleRepository.php';
+require_once __DIR__ . '/ExampleRepository2.php';
 
 $configurator->onCompile[] = function ($cf, $compiler) { $compiler->addExtension('ndbf', new Ndbf\CompilerExtension); };
 

tests/test.config.neon
@@ -8,3 +8,5 @@ nette:
 ndbf:
     repositories:
         Example: ExampleRepository
+        Example2:
+            class: ExampleRepository2